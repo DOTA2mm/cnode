f3793bd831a2700afe5f27955869fe4af93bf305
Chuck <tengchenc@gmail.com>
2019-06-26 00:31:35 +0800

    feat: user react-loadable to Code Splitting

--- config-overrides.js
+++ config-overrides.js
@@ -1,22 +1,30 @@
-const { override, fixBabelImports, addDecoratorsLegacy, addWebpackAlias, addLessLoader } = require('customize-cra');
+const {
+  override,
+  fixBabelImports,
+  addDecoratorsLegacy,
+  addWebpackAlias,
+  addLessLoader,
+  addBabelPlugin
+} = require('customize-cra');
 const path = require('path');
 
 const rewiredMap = () => config => {
-    return Object.assign(config, {devtool: config.mode === 'development' ? 'source-map' : false})
+  return Object.assign(config, { devtool: config.mode === 'development' ? 'source-map' : false });
   // config.devtool = config.mode === 'development' ? 'cheap-module-source-map' : false;
   // return config;
 };
 
 module.exports = override(
-    fixBabelImports('import', {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: true,
-    }),
-    addDecoratorsLegacy(),
-    addWebpackAlias({ '@src': path.resolve(__dirname, 'src') }),
-    addLessLoader({
-      javascriptEnabled: true,
-    }),
-    rewiredMap()
-)
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
+  }),
+  addDecoratorsLegacy(),
+  addWebpackAlias({ '@src': path.resolve(__dirname, 'src') }),
+  addLessLoader({
+    javascriptEnabled: true,
+  }),
+  rewiredMap(),
+  addBabelPlugin('@babel/plugin-syntax-dynamic-import')
+);
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import * as React from 'react';
 import { HashRouter, Route, Redirect } from 'react-router-dom';
+import Loadable from 'react-loadable';
 import Header from './components/Header/Index';
-import Home from './views/Home/Index';
-import Topic from './views/Topic/Index';
-import Profile from './views/Profile/Index';
+// import Home from './views/Home/Index';
+// import Topic from './views/Topic/Index';
+// import Profile from './views/Profile/Index';
 import './App.css';
+
+const LoadableHome = Loadable({
+  loader: () => import('./views/Home/Index'),
+  loading: () => null
+})
+const LoadableTopic = Loadable({
+  loader: () => import('./views/Topic/Index'),
+  loading: () => null
+})
+const LoadableProfile = Loadable({
+  loader: () => import('./views/Profile/Index'),
+  loading: () => null
+})
+
 
 class App extends React.PureComponent {
   render() {
@@ -13,9 +28,9 @@ class App extends React.PureComponent {
         <Header />
         <HashRouter>
           <div className="box">
-            <Route exact path="/" component={Home} />
-            <Route path="/topic/:id" component={Topic} />
-            <Route path="/user/:id" component={Profile} />
+            <Route exact path="/" component={LoadableHome} />
+            <Route path="/topic/:id" component={LoadableTopic} />
+            <Route path="/user/:id" component={LoadableProfile} />
             <Route exact path="/topic" render={() => <Redirect to="/" />} />
             <Route exact path="/user" render={() => <Redirect to="/" />} />
           </div>
